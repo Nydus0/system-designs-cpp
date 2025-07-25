b67976ecde7236a7855419eba710c93740da2736
Nydus0 <loicplantin@orange.fr>
2025-07-25 18:41:09 +0200

    feat: bimap improvements 2

--- modules/bimap/BiMap.hpp
+++ modules/bimap/BiMap.hpp
@@ -4,14 +4,20 @@
 
 #pragma once
 
+#include <algorithm>
 #include <map>
 
+struct Simple {}; struct Multi {};
+
+template<typename Key, typename Value, typename Tag = Simple>
+    requires std::totally_ordered<Key> && std::totally_ordered<Value>
+class BiMap;
+
 template <typename Key, typename Value>
-requires std::totally_ordered<Key> && std::totally_ordered<Value>
-class BiMap {
+class BiMap<Key, Value, Simple> {
 
 public:
-    void insert(Key key, Value value) {
+    void insert(const Key& key, const Value& value) {
         auto it = _originMap.find(key);
         auto it2 = _reverseMap.find(value);
 
@@ -32,7 +38,7 @@ public:
         }
     }
 
-    void remove(Key key) {
+    void remove(const Key& key) {
         auto it = _originMap.find(key);
         if (it != _originMap.end()) {
             _reverseMap.erase(it->second);
@@ -40,7 +46,7 @@ public:
         _originMap.erase(key);
     }
 
-    void remove(Value value) {
+    void remove(const Value& value) {
         auto it = _reverseMap.find(value);
         if (it != _reverseMap.end()) {
             _originMap.erase(it->second);
@@ -53,8 +59,8 @@ public:
         _reverseMap.clear();
     }
 
-    Value getMatch(const Key& key) { return _originMap.at(key); }
-    Key getMatch(const Value& value) { return _reverseMap.at(value); }
+    Value get_match(const Key& key) { return _originMap.at(key); }
+    Key get_match(const Value& value) { return _reverseMap.at(value); }
 
     bool empty() { return _originMap.empty(); }
     size_t size() { return _originMap.size(); }
@@ -62,4 +68,82 @@ public:
 private:
     std::map<Key, Value> _originMap;
     std::map<Value, Key> _reverseMap;
+};
+
+template <typename Key, typename Value>
+class BiMap<Key, Value, Multi> {
+public:
+    void insert(const Key& key, const Value& value) {
+        auto it = _originMap.find(key);
+        auto it2 = _reverseMap.find(value);
+
+        const bool key_exists = it != _originMap.end();
+        const bool value_exists = it2 != _reverseMap.end();
+
+        if (key_exists && value_exists) {
+            //do nothing
+        } else {
+            _originMap.insert({key, value});
+            _reverseMap.insert({value, key});
+        }
+    }
+
+    void remove(const Key &key) {
+        auto range = _originMap.equal_range(key);
+        for (auto [it, end_range] = _originMap.equal_range(key);
+             it != end_range; ++it) {
+            auto range2 = _reverseMap.equal_range(it->second);
+            auto it2 = std::find_if(range2.first, range2.second, [&](const auto &pair) {
+                return pair.second == key;
+            });
+            if (it2 != range2.second) {
+                _reverseMap.erase(it2);/**/
+            }
+        }
+        _originMap.erase(key);
+    }
+
+    void remove(const Value &value) {
+        auto range = _reverseMap.equal_range(value);
+        for (auto [it, end_range] = _reverseMap.equal_range(value);
+             it != end_range; ++it) {
+            auto range2 = _originMap.equal_range(it->second);
+            auto it2 = std::find_if(range2.first, range2.second, [&](const auto &pair) {
+                return pair.second == value;
+            });
+            if (it2 != range2.second) {
+                _originMap.erase(it2);
+            }
+        }
+        _reverseMap.erase(value);
+    }
+
+    void remove(const Key &key, const Value &value) {
+        auto range = _originMap.equal_range(key);
+        auto it = std::find_if(range.first, range.second, [&](const auto &pair) {
+            return pair.second == value;
+        });
+        if (it != range.second) {
+            _originMap.erase(it);
+        }
+
+        auto range2 = _reverseMap.equal_range(value);
+        auto it2 = std::find_if(range2.first, range2.second, [&](const auto &pair) {
+            return pair.second == key;
+        });
+        if (it2 != range2.second) {
+            _reverseMap.erase(it2);
+        }
+    }
+
+    auto get_match_equal_range(const Key& key) { return _originMap.equal_range(key); }
+    auto get_match_equal_range(const Value& value) { return _reverseMap.equal_range(value); }
+
+    bool empty() { return _originMap.empty(); }
+    size_t size() { return _originMap.size(); }
+
+private:
+    std::multimap<Key, Value> _originMap;
+    std::multimap<Value, Key> _reverseMap;
+
 };
--- tests/bimap.cpp
+++ tests/bimap.cpp
@@ -6,16 +6,18 @@
 
 #include <gtest/gtest.h>
 
-TEST(bimap, insert) {
+TEST(bimap, simple_insert) {
     BiMap<std::string, int> map;
+
     map.insert("obj1", 1);
     map.insert("obj2", 2);
     map.insert("obj3", 3);
     EXPECT_EQ(map.size(), 3);
 }
 
-TEST(bimap, remove) {
+TEST(bimap, simple_remove) {
     BiMap<std::string, int> map;
+
     map.insert("obj1", 1);
     map.insert("obj2", 2);
     map.insert("obj3", 3);
@@ -24,16 +26,18 @@ TEST(bimap, remove) {
     EXPECT_EQ(map.size(), 1);
 }
 
-TEST (bimap, clear) {
+TEST (bimap, simple_clear) {
     BiMap<std::string, int> map;
+
     map.insert("obj1", 1);
     map.insert("obj2", 2);
     map.insert("obj3", 3);
     map.clear();
 }
 
-TEST (bimap, empty) {
+TEST (bimap, simple_empty) {
     BiMap<std::string, int> map;
+
     EXPECT_EQ(map.empty(), true);
     map.insert("obj1", 1);
     EXPECT_EQ(map.empty(), false);
@@ -43,14 +47,88 @@ TEST (bimap, empty) {
     EXPECT_EQ(map.empty(), true);
 }
 
-TEST (bimap, getMatch) {
+TEST (bimap, simple_get_match) {
     BiMap<std::string, int> map;
+
     map.insert("obj1", 1);
     map.insert("obj2", 2);
-
-    EXPECT_EQ(map.getMatch("obj1"), 1);
-    EXPECT_EQ(map.getMatch(1), "obj1");
-    EXPECT_EQ(map.getMatch("obj2"), 2);
-    EXPECT_EQ(map.getMatch(2), "obj2");
+    EXPECT_EQ(map.get_match("obj1"), 1);
+    EXPECT_EQ(map.get_match(1), "obj1");
+    EXPECT_EQ(map.get_match("obj2"), 2);
+    EXPECT_EQ(map.get_match(2), "obj2");
 }
+
+TEST (bimap, multi_insert) {
+    BiMap<std::string, int, Multi> bimap;
+
+    bimap.insert("link11", 1);
+    bimap.insert("link11", 2);
+    EXPECT_EQ(bimap.size(), 2);
+    bimap.insert("link22", 2);
+    bimap.insert("link32", 2);
+    EXPECT_EQ(bimap.size(), 4);
+    bimap.insert("link22", 2);
+    EXPECT_EQ(bimap.size(), 4);
+    bimap.insert("link33", 3);
+    EXPECT_EQ(bimap.size(), 5);
+}
+
+TEST (bimap, multi_remove_value) {
+    BiMap<std::string, int, Multi> bimap;
+
+    bimap.insert("link11", 1);
+    bimap.insert("link11", 2);
+    bimap.insert("link22", 2);
+    bimap.insert("link32", 2);
+    bimap.insert("link22", 2);
+    bimap.insert("link33", 3);
+    EXPECT_EQ(bimap.size(), 5);
+    bimap.remove(2);
+    EXPECT_EQ(bimap.size(), 2);
+}
+
+TEST (bimap, multi_remove_key) {
+    BiMap<std::string, int, Multi> bimap;
+
+    bimap.insert("link11", 1);
+    bimap.insert("link11", 2);
+    bimap.insert("link22", 2);
+    bimap.insert("link32", 2);
+    bimap.insert("link22", 4);
+    bimap.insert("link33", 3);
+    EXPECT_EQ(bimap.size(), 6);
+    bimap.remove("link22");
+    EXPECT_EQ(bimap.size(), 4);
+}
+
+TEST (bimap, multi_remove_pair) {
+    BiMap<std::string, int, Multi> bimap;
+
+    bimap.insert("link11", 1);
+    bimap.insert("link11", 2);
+    bimap.insert("link22", 2);
+    bimap.insert("link32", 2);
+    bimap.insert("link33", 3);
+    EXPECT_EQ(bimap.size(), 5);
+    bimap.remove("link22", 2);
+    EXPECT_EQ(bimap.size(), 4);
+}
+
+TEST (bimap, multi_get_match_equal_range) {
+    BiMap<std::string, int, Multi> bimap;
+    bimap.insert("link11", 1);
+    bimap.insert("link11", 2);
+    bimap.insert("link22", 2);
+    bimap.insert("link32", 2);
+    bimap.insert("link33", 3);
+
+    auto [it_begin, it_end] = bimap.get_match_equal_range("link11");
+    EXPECT_EQ(it_begin->second, 1);
+    EXPECT_EQ(std::next(it_begin, 1)->second, 2);
+    auto [it_begin2, it_end2] = bimap.get_match_equal_range(2);
+    EXPECT_EQ(it_begin2->second, "link11");
+    EXPECT_EQ(std::next(it_begin2, 1)->second, "link22");
+    EXPECT_EQ(std::next(it_begin2, 2)->second, "link32");
+}
+
 
